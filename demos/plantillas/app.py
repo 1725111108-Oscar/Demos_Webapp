6932bb19a9b3595c8decd7012dae5f1606d06a79
1725111108-Oscar <1725111108@utectulancingo.edu.mx>
2026-06-01 13:38:02 +0000

    CREATED configuracion del virtual enviroment

--- demos/plantillas/app.py
+++ demos/plantillas/app.py
@@ -1,14 +1,23 @@
 import web
-
 urls = (
-    '/', 'Index'
+    '/', 'Index',
+    '/clientes', 'clientes',
+    '/usuarios','usuarios',
 )
 app = web.application(urls, globals())
-render = web.template.render('templates')
+render = web.template.render('templates/')
 
 class Index:
     def GET(self):
-        return render.index()
+        return str(render.index())
+   
 
+class Clientes:
+    def GET(self):
+        return str(render.clientes())
+class Usuario:
+    def GET(self):
+        return str(render.usuarios())   
+     
 if __name__ == "__main__":
     app.run()
