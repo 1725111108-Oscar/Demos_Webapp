51bd53c387fae7cb81bd46a87553839ac94e5916
1725111108-Oscar <1725111108@utectulancingo.edu.mx>
2026-06-12 19:37:28 +0000

    CREATED configuracion del virtual enviroment

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -1,4 +1,5 @@
 import web
+import math
 urls = (
     '/', 'Index',
     '/calculadora', 'Calculadora'
@@ -18,34 +19,40 @@ class Calculadora:
         numero_2=0.0
         resultado=0.0
         return render.calculadora(numero_1, numero_2, resultado)
-
+    
     def POST(self):
         formulario = web.input()
-        numero_1 = float(formulario['numero_1'])
-        numero_2 = float(formulario['numero_2'])
+        numero_1 = float(formulario ['numero_1'])
+        numero_2 = float(formulario ['numero_2'])
         resultado = numero_1 + numero_2
 
-    def POST(self):
-        formulario = web.input()
-        numero_1 = float(formulario['numero_1'])
-        numero_2 = float(formulario['numero_2'])
-        resultado = numero_1 - numero_2
+        operacion = formulario.get('operacion')
+        if operacion == "sumar":
+            resultado = numero_1 + numero_2
+        elif operacion == "restar":
+            resultado = numero_1 - numero_2    
+        elif operacion ==  "multiplicar":
+            resultado = numero_1 * numero_2
+        elif operacion == "dividir":
+            if numero_2 !=0:
+                resultado = numero_1 / numero_2
+        elif operacion == "raiz cuadrada":
+            if numero_1 >=0:
+                resultado = math.sqrt(numero_1)
+        elif operacion == "potencia":
+            resultado = numero_1 ** numero_2  
+        elif operacion =="modulo":
+            resultado = numero_1 % numero_2
+                 
 
-    def POST(self):
-        formulario = web.input()
-        numero_1 = float(formulario['numero_1'])
-        numero_2 = float(formulario['numero_2'])
-        resultado = numero_1 * numero_2
 
-    def POST(self):
-        formulario = web.input()
-        numero_1 = float(formulario['numero_1'])
-        resultado = numero_1 ** (1/2)
+    
 
-        print(f"Tipo de dato de numero_1 : {type(numero_1)}")
-        print(f"Tipo de dato de numero_2 : {type(numero_2)}")
+        print(f"Tipo de dato de numero_1: {type(numero_1)}")
+        print(f"Tipo de dato de numero_2: {type(numero_2)}")
 
         return render.calculadora(numero_1, numero_2, resultado)
-
+  
+    
 if __name__ == "__main__":
     app.run()
